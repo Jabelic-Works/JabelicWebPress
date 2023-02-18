86ae609c79309c88e744dabdf12fbf01d1faede4
jabelic <hibiki990523@gmail.com>
2023-02-19 02:39:35 +0900

    ADD: favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,5 +13,17 @@ export default defineNuxtConfig({
     }
   },
   css: ['~/assets/style/_reset.css', '~/assets/style/index.css'],
-  modules: ['@pinia/nuxt']
+  modules: ['@pinia/nuxt'],
+  app:{
+    head: {
+      title: "Jabelic Web Press",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+      link: [
+        { rel: "icon", type: "image/png", href: "/icon-16x16.png" }, // これを追記する
+      ],
+    },
+  }
 })
