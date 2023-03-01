d9746ec26c137626e685ec937ee3c033cf1ef7b9
jabelic <hibiki990523@gmail.com>
2023-03-02 01:43:02 +0900

    FIX: menu

--- composables/articles.ts
+++ composables/articles.ts
@@ -1,39 +1,39 @@
-import { createClient, MicroCMSListResponse } from 'microcms-js-sdk';
+// import { createClient, MicroCMSListResponse } from 'microcms-js-sdk';
 
-export const useArticle = async (apiKey: string)=>{
-    type Article = {
-        title:string;
-        description: string;
-        tags: Array<string> | null,
-        to: string
-    }
-    const getArticleList = async () => {
-        try {
-            if(!apiKey) return
-            const { data, pending, error, refresh } = await useFetch<MicroCMSListResponse<any>>("https://jabelicwebpress.microcms.io/api/v1/blogs", {
-                headers:{
-                    "X-MICROCMS-API-KEY": apiKey
-                }
-            })
-            return data
-        } catch (error) {
-            console.error(error)
-        }
-    }
+// export const useArticle = async (apiKey: string)=>{
+//     type Article = {
+//         title:string;
+//         description: string;
+//         tags: Array<string> | null,
+//         to: string
+//     }
+//     const getArticleList = async () => {
+//         try {
+//             if(!apiKey) return
+//             const { data, pending, error, refresh } = await useFetch<MicroCMSListResponse<any>>("https://jabelicwebpress.microcms.io/api/v1/blogs", {
+//                 headers:{
+//                     "X-MICROCMS-API-KEY": apiKey
+//                 }
+//             })
+//             return data
+//         } catch (error) {
+//             console.error(error)
+//         }
+//     }
 
-    const articles = ref<Array<Article>>([
-        {
-            title: "たいとる",
-            description: "せつめい",
-            tags: ["DevOps", "TypeScript", "Julia"],
-            to: '/article/1'
-        },
-        {
-            title: "たいとる2",
-            description: "せつめいせつめいせつめいせつめいせつめいせつめいせつめいせつめいせつめいせつめいせつめいせつめいせつめいせつめいせつめいせつめいせつめいせつめい",
-            tags: null,
-            to: '/article/2'
-        }
-    ])
-    return { articles, getArticleList}
-}
+//     const articles = ref<Array<Article>>([
+//         {
+//             title: "たいとる",
+//             description: "せつめい",
+//             tags: ["DevOps", "TypeScript", "Julia"],
+//             to: '/article/1'
+//         },
+//         {
+//             title: "たいとる2",
+//             description: "せつめいせつめいせつめいせつめいせつめいせつめいせつめいせつめいせつめいせつめいせつめいせつめいせつめいせつめいせつめいせつめいせつめいせつめい",
+//             tags: null,
+//             to: '/article/2'
+//         }
+//     ])
+//     return { articles, getArticleList}
+// }
